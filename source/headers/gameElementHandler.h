6262bbb29b22dac90b954b139e74f06a4b489216
simonsvale <s.risbjerg@stofanet.dk>
2023-10-29 20:40:21 +0100

    Added a new class called GameElement, which stores the information about each game element when loading in the .arrmap file.

---
+++ source/headers/gameElementHandler.h
@@ -0,0 +1,23 @@
+#ifndef GAME_ELEMENT_HANDLER_H
+#define GAME_ELEMENT_HANDLER_H
+
+#include <string>
+
+#include <graphicsHandler.h>
+
+
+class GameElement: public Graphics
+{
+    public:
+        // All variables that need to be set using the .arrmap file.
+        int Type;
+
+        // Only here to allow change of textures on the fly.
+        string VertexShaderPath = NULL;
+        string FragmentShaderPath = NULL;
+
+        // Because its a Graphics Object, it also includes VAO.
+
+};
+
+#endif
