d3408a1de8ce3fa9d5a2a5360ae403596d98bf26
simonsvale <s.risbjerg@stofanet.dk>
2023-12-13 23:52:28 +0100

    Put a hold on real time reflections and started working on importing and using cubemaps for static reflection.

--- source/code/cubemaps.cpp
+++ source/code/cubemaps.cpp
@@ -1,16 +1,13 @@
 #include "cubemaps.h"
 
+using namespace std;
 
-
-void create_reflection_framebuffer(GLuint *FrameBufferObject, GLuint *Texture)
+void Cubemap::create_reflection_cubemap(void)
 {
-    unsigned int CUBEMAP_RES_W, CUBEMAP_RES_H = 1024;
-
     // Generate the framebuffer and the cubemap texture.
-    glGenFramebuffers(1, FrameBufferObject);
-    glGenTextures(1, Texture);
+    glGenTextures(1, &Texture);
 
-    glBindTexture(GL_TEXTURE_CUBE_MAP, *Texture);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, Texture);
 
     // Assign a 2d texture to each side of the cubemap.
     for (unsigned int i = 0; i < 6;)
@@ -26,10 +23,82 @@ void create_reflection_framebuffer(GLuint *FrameBufferObject, GLuint *Texture)
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 
-    // Bind the framebuffer and cubemap texture.
-    glBindFramebuffer(GL_FRAMEBUFFER, *FrameBufferObject);
-    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, *Texture, 0);
+    // Bind the cubemap texture.
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, Texture, 0);
 
-    // Unbind the framebuffer.
+    // Unbind the cubemap texture 
+    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+}
+
+void Cubemap::render_reflection_framebuffer(Shader ReflectionShader)
+{   
+    // Set the viewport size the framebuffer should render to.
+    glViewport(0, 0, CUBEMAP_RES_H, CUBEMAP_RES_W);
+    glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(ReflectionShader.ShaderProgram);
+
+    vector<glm::mat4> CubeSides;
+    CubeSides.reserve(6);
+
+    float CloseReflection = 0.1;
+    float FarReflection = 25.0;
+    glm::mat4 Cubeprojection = glm::perspective(glm::radians(90.0f), (float)1024 / (float)1024, CloseReflection, FarReflection);
+
+    // Look at all four cardinal directions, and up, down. Basically just the sides of the cube the cubemap consists of.
+    CubeSides.push_back(Cubeprojection * glm::lookAt(CubePos, CubePos + glm::vec3( 1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)));
+    CubeSides.push_back(Cubeprojection * glm::lookAt(CubePos, CubePos + glm::vec3(-1.0f,  0.0f,  0.0f), glm::vec3(0.0f, -1.0f,  0.0f)));
+    CubeSides.push_back(Cubeprojection * glm::lookAt(CubePos, CubePos + glm::vec3( 0.0f,  1.0f,  0.0f), glm::vec3(0.0f,  0.0f,  1.0f)));
+    CubeSides.push_back(Cubeprojection * glm::lookAt(CubePos, CubePos + glm::vec3( 0.0f, -1.0f,  0.0f), glm::vec3(0.0f,  0.0f, -1.0f)));
+    CubeSides.push_back(Cubeprojection * glm::lookAt(CubePos, CubePos + glm::vec3( 0.0f,  0.0f,  1.0f), glm::vec3(0.0f, -1.0f,  0.0f)));
+    CubeSides.push_back(Cubeprojection * glm::lookAt(CubePos, CubePos + glm::vec3( 0.0f,  0.0f, -1.0f), glm::vec3(0.0f, -1.0f,  0.0f)));
+
+    string ReflectionMatrix;
+    for (unsigned int i = 0; i < 6;)
+    {
+        ReflectionMatrix = "CubeSides[" + std::to_string(i) + "]";
+        glUniformMatrix4fv( glGetUniformLocation(ReflectionShader.ShaderProgram, ReflectionMatrix.c_str()), 1, GL_FALSE, &CubeSides[i][0][0]);
+        i++;
+    }
+
+    // Steps:
+
+    // Precompute cubemaps as textures and store them somewhere.
+
+
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+}
+
+
+void Cubemap::load_cubemap(void)
+{
+    // Generate the framebuffer and the cubemap texture.
+    glGenTextures(1, &Texture);
+
+    glBindTexture(GL_TEXTURE_CUBE_MAP, Texture);
+
+    int ImageWidth;
+    int ImageHeight;
+    int Channels;
+
+    // Assign a 2d texture to each side of the cubemap.
+    for (unsigned int i = 0; i < 6;)
+    {   
+        unsigned char* ImageData = stbi_load(CubemapPath[i].c_str(), &ImageWidth, &ImageHeight, &Channels, 0);
+
+        // This is possible due to GL_TEXTURE_CUBE_MAP_POSITIVE_X's hex value being 1 int from every other side.
+        glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA, CUBEMAP_RES_W, CUBEMAP_RES_H, 0, GL_RGBA, GL_UNSIGNED_BYTE, ImageData);
+
+        stbi_image_free(ImageData);
+        i++;
+    }
+    
+    // Setup parameters
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 }
--- source/headers/cubemaps.h
+++ source/headers/cubemaps.h
@@ -1,8 +1,17 @@
 #ifndef CUBEMAPS_H
 #define CUBEMAPS_H
 
-#include <glad/glad.h>
 #include <SDL2/SDL.h>
+#include <glad/glad.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <vector>
+#include <string>
+#include <array>
+
+#include "shaderHandler.h"
 
 class Cubemap
 {
@@ -10,7 +19,21 @@ class Cubemap
         GLuint Texture;
         GLuint FBO;
 
-        void create_reflection_framebuffer(GLuint *FrameBufferObject, GLuint *Texture);
+        array<string, 6> CubemapPath;
+        
+        // Texture size of each face.
+        GLuint CUBEMAP_RES_W = 1024;
+        GLuint CUBEMAP_RES_H = 1024;
+        
+        // Position of the cubemap.
+        glm::vec3 CubePos;
+
+        void create_reflection_cubemap(void);
+
+        void render_reflection_framebuffer(Shader ReflectionShader);
+
+        void load_cubemap(void);
+
 
 
 
